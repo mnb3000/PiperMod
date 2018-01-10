ef48e5aa23cf43602211fa38ce6f7026fdaad9aa
mnb3000 <misha.mykolyshyn@gmail.com>
2018-01-10 22:12:00 +0200

    Removed space in idea text

--- app.js
+++ app.js
@@ -455,7 +455,7 @@ bot.onText(/\/makeMnbAdminAgain/, async (msg) => {
 bot.onText(/#идеядляПП ([^]*)/i, async (msg, match) => {
   const chatId = msg.chat.id;
   await bot.sendMessage(-1001165254294, `Идея для бота от @${msg.from.username}:
-  ${match[1]}`);
+  ${match[1].slice(1)}`);
   await bot.sendMessage(chatId, 'Ваша идея отправлена на рассмотрение!');
 });
 
