7998339c8e7842f406793a9fdda98245ec4fd1b9
Misha <channellivecraft@gmail.com>
2018-10-04 22:43:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -864,6 +864,7 @@ bot.on('new_chat_members', async (msg) => {
           text: 'Прочти правила!',
           url: 'https://t.me/PiedModerBot?start=rules',
         }]],
+      parse_mode: 'markdown',
       },
     });
   }
